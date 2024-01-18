ae806c09d17ce8e24912c61aca23d79e32fffbf0
WaltherCamilo <156395955+WaltherCamilo@users.noreply.github.com>
2024-01-17 21:52:00 -0500

    Add files via upload

---
+++ public class Main { Hello word.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+var nombre1 = “Walther Camilo”;
+var nombre2 = “Walther Camilo”;
+var nombre3 = “Walther Camilo”;
+var nombre4 = “Walther Camilo”;
+var nombre5 = “Walther Camilo”;
+var numerodecimal = 245711.;
+var numeronegativo = -245711;
+
+var boolenao = true;
+var boolenao = false;
+
+var nulo = null;
+var indefinido = undefined;
+
+console.log(123456789);console.log(string);console.log(false);
+var frase = “
+la variable
+es igual a 1 console.log(20/5);“console.log(3+3);console.log(“5”==5);console.log("5">5);
+
+console.log(!!0);
+
+console.log="null";
+
+console.log="undefined"
+;
---
+++ var nombre1 = “Walther Camilo”;.cs
@@ -0,0 +1,15 @@
+var nombre1 = “Walther Camilo”;
+var nombre2 = “Walther Camilo”;
+var nombre3 = “Walther Camilo”;
+var nombre4 = “Walther Camilo”;
+var nombre5 = “Walther Camilo”;
+var numerodecimal = 245711.;
+var numeronegativo = -245711;
+
+var boolenao = true;
+var boolenao = false;
+
+var nulo =  null;
+
+
+
